280ec567f1cd745afe8ccee1ebbf564c740612d7
Younes <younes.boutkrida@telecom-sudparis.eu>
2026-02-03 14:01:17 +0000

    push

--- TP1/src/quick_test_sam.py
+++ TP1/src/quick_test_sam.py
@@ -8,7 +8,7 @@ img_path = next(Path("TP1/data/images").glob("*.jpeg"))  # adaptez si besoin
 bgr = cv2.imread(str(img_path), cv2.IMREAD_COLOR)
 rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
 
-ckpt = "TP1/models/_______"  # nom du fichier .pth
+ckpt = "TP1/models/sam_vit_h_4b8939.pth"  # nom du fichier .pth
 pred = load_sam_predictor(ckpt, model_type="vit_h") # Attention, ou vit_l ou vit_b
 
 # bbox "à la main" (exemple) : adaptez à votre image
