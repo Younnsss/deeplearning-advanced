dc5ebbdba5a293ac7073d809399977913e27176b
Younes <younes.boutkrida@telecom-sudparis.eu>
2026-02-23 10:32:40 +0000

    psu

--- TP3/inspect_audio.py
+++ TP3/inspect_audio.py
@@ -9,9 +9,9 @@ def clipping_rate(x: torch.Tensor, thr: float = 0.99) -> float:
 
 def main():
     path = "TP3/data/call_01.wav"
-    wav, sr = torchaudio.load(path, backend="soundfile")          # wav: [channels, time]
+    wav, sr = torchaudio.load(path)          # wav: [channels, time]
     wav = wav.mean(dim=0, keepdim=True)      # force mono [1, time]
-    num_samples = wav.shape[1]
+    num_samples = wav.shape[0]
     duration_s = num_samples / sr
 
     print("path:", path)
