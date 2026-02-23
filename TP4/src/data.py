e60c33d9a8cc00e616fdf4a2ad0a78be73856e3b
Younes <younes.boutkrida@telecom-sudparis.eu>
2026-02-23 15:00:05 +0000

    push

--- TP4/src/data.py
+++ TP4/src/data.py
@@ -1,14 +1,17 @@
-# TP4/src/data.py
+# TP4/src/data.py (mise à jour)
 import os
 from dataclasses import dataclass
 import torch
 from torch_geometric.datasets import Planetoid
+from torch_geometric.data import Data
 
 
 @dataclass
 class CoraData:
+    pyg_data: Data
     x: torch.Tensor
     y: torch.Tensor
+    edge_index: torch.Tensor
     train_mask: torch.Tensor
     val_mask: torch.Tensor
     test_mask: torch.Tensor
@@ -22,8 +25,10 @@ def load_cora() -> CoraData:
     data = dataset[0]
 
     return CoraData(
+        pyg_data=data,
         x=data.x,
         y=data.y,
+        edge_index=data.edge_index,
         train_mask=data.train_mask,
         val_mask=data.val_mask,
         test_mask=data.test_mask,
